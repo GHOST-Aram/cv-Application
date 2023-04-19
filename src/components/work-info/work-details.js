864523d25efedcd725170e7e8b259294cfeb7c7b
GHOST-Aram <owinoojala@gmail.com>
2023-04-19 20:42:11 +0300

    Chore: Correct heading

--- src/components/work-info/work-details.js
+++ src/components/work-info/work-details.js
@@ -5,7 +5,7 @@ import EditButton from '../edit-button'
 const WorkDetails = ({workExp, handleClick}) => {
   return (
     <div className='container-2 text-left'>
-            <h2 className="font-light text-center py-8">Education Experience</h2>
+            <h2 className="font-light text-center py-8">Work Experience</h2>
             <div className='grid grid-3 gap-8 py-8'>
             <Data label='Company' value={workExp.company}/><Data label='Job Title' value={workExp.jobTitle}/><Data label='Role' value={workExp.role}/>
             <Data label='StartDate' value={workExp.startDate}/><Data label='End Date' value={workExp.endDate}/>
